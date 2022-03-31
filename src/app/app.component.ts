0390569f949c68fbb20032c1c2bfb476f34f6623
Nicolas Ramirez <niramirez@ext.prevencionart.com.ar>
2022-03-31 19:57:03 -0300

    se agrega codigo para reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      edad: ['', Validators.compose([
+        Validators.required, 
+        Validators.min(1),
+        Validators.max(180)
+      ])],
+      empresa: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])]
+    });
+  }
+
+  submit(): void {
+    console.log(JSON.stringify(this.form.value));
+  }
 }
